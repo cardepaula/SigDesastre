24f4b2034c10f33604da9c18bfd653b36d01dbb6
David M. Marques <mouradavid8@gmail.com>
2019-09-10 11:34:42 -0300

    chore(CORS): disabling cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { appConfig } from './common/config/app.config';
 import * as pacote from '../package.json';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
-  // app.enableCors({ origin: '*' });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
 
   const options = new DocumentBuilder();
   options.setTitle(pacote.name);
