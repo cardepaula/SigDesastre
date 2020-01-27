788dd38226b8df5924e0323087a4541830316ab2
Marcos A C Paula <cardepaula@gmail.com>
2020-01-27 15:48:13 -0300

    feat(tweets): Add tweets module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { NoticiaModule } from './noticia/noticia.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [DatabaseModule, NoticiaModule],
+  imports: [DatabaseModule, NoticiaModule, TweetsModule],
   controllers: [],
   providers: [],
 })
