22288adc88ecf35630f59b037f99ee21fe4f308f
Marcos A C Paula <cardepaula@gmail.com>
2020-01-27 19:51:19 -0300

    fix(import): Fix import path

--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TweetsController } from './tweets.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { TweetsService } from './tweets.service';
 
 @Module({
