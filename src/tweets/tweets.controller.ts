cd288d4dbb9baf5446150e496ee8ac97e0706526
Marcos A C de Paula <cardepaula@gmail.com>
2020-06-21 16:14:50 -0300

    refactor(tweets): Tweets module to nestjsx/crud

--- src/tweets/tweets.controller.ts
+++ src/tweets/tweets.controller.ts
@@ -1,37 +1,28 @@
-import { Controller, Get, HttpStatus, Query } from '@nestjs/common';
-import { ApiUseTags, ApiImplicitQuery, ApiResponse } from '@nestjs/swagger';
+import {
+  Controller,
+} from '@nestjs/common';
+import {
+  ApiUseTags,
+} from '@nestjs/swagger';
 import { TweetsService } from './tweets.service';
-import { TweetsDto } from './dto/tweets.dto';
+import { TweetsDto, CreateTweetsDto } from './dto';
+import { Tweets } from '../database/entities/tweets.entity';
+import { CrudController, Crud } from '@nestjsx/crud';
 
+@Crud({
+  model: {
+    type: TweetsDto,
+  },
+  dto: {
+    create: CreateTweetsDto,
+  },
+})
 @ApiUseTags('Tweets')
 @Controller('tweets')
-export class TweetsController {
-  constructor(private readonly tweetsService: TweetsService) {}
+export class TweetsController implements CrudController<Tweets> {
+  constructor(public service: TweetsService) {}
 
-  @ApiImplicitQuery({
-    name: 'qtdTweets',
-    description:
-      'Quantidade de tweets que deseja buscar por request, por padrão o valor é 10',
-    required: false,
-    type: Number,
-    isArray: false,
-    collectionFormat: 'multi',
-  })
-  @ApiImplicitQuery({
-    name: 'pagina',
-    description: 'pagina que deseja visualizar, por padrão o valor é 1',
-    required: false,
-    type: Number,
-    isArray: false,
-    collectionFormat: 'multi',
-  })
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'Tweets Filtradas',
-    type: TweetsDto
-  })
-  @Get()
-  public search(@Query() query) {
-    return this.tweetsService.searchTweets(query);
+  get base(): CrudController<Tweets> {
+    return this;
   }
 }
