ed5ae5efea890dc1b26db79105abfdf9c1ce9714
Marcos A C de Paula <cardepaula@gmail.com>
2020-07-10 02:05:18 -0300

    refactor(crud): Update crudGlobalConfig

--- src/common/config/crud.config.ts
+++ src/common/config/crud.config.ts
@@ -2,9 +2,7 @@ import { CrudGlobalConfig } from '@nestjsx/crud';
 
 export const crudGlobalConfig: CrudGlobalConfig = {
   query: {
-    maxLimit: 10000,
     cache: 2000,
-    alwaysPaginate: true,
   },
   routes: {
     exclude: ['replaceOneBase'],
